9aee3daa2ef96a4c1d697661af6b02f5c5da12e2
interrrp <watchyourcache@gmail.com>
2024-12-20 19:30:34 +0800

    feat(cpu): support `JP` instruction

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,17 +1,19 @@
 use crate::instruction::Instruction;
 
-pub(crate) struct Chip8<'a> {
-    pub registers: Box<&'a [u8; 15]>,
-    pub stack: Box<&'a [u16; 16]>,
+pub(crate) struct Chip8 {
+    pub registers: [u8; 15],
+    pub stack: [u16; 16],
+    pub sp: usize,
     pub program: Vec<Instruction>,
-    pub pc: u16,
+    pub pc: usize,
 }
 
-impl<'a> Chip8<'a> {
-    pub fn new() -> Chip8<'a> {
+impl Chip8 {
+    pub fn new() -> Chip8 {
         Chip8 {
-            registers: Box::new(&[0; 15]),
-            stack: Box::new(&[0; 16]),
+            registers: [0; 15],
+            stack: [0; 16],
+            sp: 0,
             program: Vec::new(),
             pc: 0,
         }
@@ -21,7 +23,23 @@ impl<'a> Chip8<'a> {
         self.program = program;
     }
 
-    pub fn run(&mut self) {}
+    pub fn run(&mut self) {
+        while self.pc < self.program.len() {
+            self.do_instruction(self.program[self.pc]);
+        }
+    }
+
+    fn do_instruction(&mut self, instruction: Instruction) {
+        match instruction {
+            Instruction::Jp { addr } => self.pc = addr as usize,
+            _ => {}
+        }
+
+        // Only increment program counter if the instruction isn't a JP (jump)
+        if !matches!(instruction, Instruction::Jp { .. }) {
+            self.pc += 1;
+        }
+    }
 }
 
 #[cfg(test)]
@@ -35,5 +53,15 @@ mod tests {
         let mut chip8 = Chip8::new();
         chip8.load_program(program.clone());
         assert_eq!(chip8.program, program);
+    }
+
+    #[test]
+    fn jp() {
+        let mut chip8 = Chip8::new();
+        chip8.load_program(vec![Instruction::Jp { addr: 0xa }]);
+
+        assert_eq!(chip8.pc, 0);
+        chip8.run();
+        assert_eq!(chip8.pc, 0xa);
     }
 }
