7e99c12342a86d50f70ce1179dbf7cea679c0e63
interrrp <watchyourcache@gmail.com>
2024-12-27 08:48:51 +0800

    fix: timing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{fs, path::PathBuf, thread::sleep, time::Duration};
 
 use clap::Parser;
-use raylib::{color::Color, ffi::KeyboardKey, prelude::RaylibDraw};
+use raylib::{color::Color, ffi::KeyboardKey, prelude::RaylibDraw, RaylibHandle};
 
 /// A tiny CHIP-8 emulator.
 #[derive(Parser, Debug)]
@@ -95,158 +95,164 @@ impl Emulator {
             .build();
 
         while self.should_run() {
-            let opcode = self.fetch_opcode();
-
-            let v = &mut self.registers;
-
-            let x = ((opcode & 0x0F00) >> 8) as usize;
-            let y = ((opcode & 0x00F0) >> 4) as usize;
-            let n = opcode & 0x000F;
-            let nn = (opcode & 0x00FF) as u8;
-            let nnn = (opcode & 0x0FFF) as usize;
-
-            if self.dt > 0 {
-                self.dt -= 1;
-                continue;
+            for _ in 0..11 {
+                self.execute_cycle(&mut rl);
             }
 
-            match (opcode & 0xF000) >> 12 {
-                0x0 if nn == 0x00 => {}
-                0x0 if nn == 0xE0 => self.display = [false; DISPLAY_WIDTH * DISPLAY_HEIGHT],
-                0x0 if nn == 0xEE => match self.stack.pop() {
-                    Some(addr) => self.pc = addr,
-                    None => println!("RET outside subroutine"),
-                },
-                0x1 => self.pc = nnn - 2,
-                0x2 => {
-                    self.stack.push(self.pc);
-                    self.pc = nnn - 2;
-                }
-                0x3 if v[x] == nn => self.pc += 2,
-                0x3 if v[x] != nn => {}
-                0x4 if v[x] != nn => self.pc += 2,
-                0x4 if v[x] == nn => {}
-                0x5 if n == 0 && v[x] == v[y] => self.pc += 2,
-                0x5 if n == 0 && v[x] != v[y] => {}
-                0x6 => v[x] = nn,
-                0x7 => v[x] = v[x].wrapping_add(nn),
-                0x8 if n == 0x0 => v[x] = v[y],
-                0x8 if n == 0x1 => v[x] |= v[y],
-                0x8 if n == 0x2 => v[x] &= v[y],
-                0x8 if n == 0x3 => v[x] ^= v[y],
-                0x8 if n == 0x4 => {
-                    let (vx, carry) = v[x].overflowing_add(v[y]);
-                    v[x] = vx;
-                    v[0xF] = carry.into();
-                }
-                0x8 if n == 0x5 => {
-                    let vf = (v[x] >= v[y]).into();
-                    v[x] = v[x].wrapping_sub(v[y]);
-                    v[0xF] = vf;
-                }
-                0x8 if n == 0x6 => {
-                    let vf = v[y] & 1;
-                    v[y] >>= 1;
-                    v[0xF] = vf;
-                }
-                0x8 if n == 0x7 => {
-                    let vf = (v[y] >= v[x]).into();
-                    v[x] = v[y].wrapping_sub(v[x]);
-                    v[0xF] = vf;
-                }
-                0x8 if n == 0xE => {
-                    let vf = v[y] >> 7;
-                    v[y] <<= 1;
-                    v[0xF] = vf;
-                }
-                0x9 if n == 0x0 && v[x] != v[y] => self.pc += 2,
-                0x9 if n == 0x0 && v[x] == v[y] => {}
-                0xA => self.i = nnn,
-                0xB => self.pc = (nnn + v[0] as usize) - 2,
-                0xC => v[x] = fastrand::u8(0..u8::MAX) & nn,
-                0xD => {
-                    v[0xF] = 0;
-                    let x = v[x] as usize;
-                    let y = v[y] as usize;
+            let mut d = rl.begin_drawing(&rl_thread);
+            d.clear_background(Color::BLACK);
 
-                    for row in 0..n {
-                        if (y + row as usize) >= DISPLAY_HEIGHT {
-                            break;
-                        }
-
-                        let sprite_byte = self.memory[self.i + row as usize];
-                        for bit in 0..8 {
-                            if (x + bit) >= DISPLAY_WIDTH {
-                                continue;
-                            }
-
-                            if (sprite_byte & (0x80 >> bit)) != 0 {
-                                let idx = (y + row as usize) * DISPLAY_WIDTH + (x + bit);
-                                if self.display[idx] {
-                                    v[0xF] = 1;
-                                }
-                                self.display[idx] ^= true;
-                            }
-                        }
-                    }
-
-                    let mut d = rl.begin_drawing(&rl_thread);
-                    d.clear_background(Color::BLACK);
-
-                    for y in 0..DISPLAY_HEIGHT {
-                        for x in 0..DISPLAY_WIDTH {
-                            d.draw_rectangle(
-                                (x * DISPLAY_SCALE) as i32,
-                                (y * DISPLAY_SCALE) as i32,
-                                DISPLAY_SCALE as i32,
-                                DISPLAY_SCALE as i32,
-                                if self.display[y * DISPLAY_WIDTH + x] {
-                                    Color::WHITE
-                                } else {
-                                    Color::BLACK
-                                },
-                            );
-                        }
-                    }
+            for y in 0..DISPLAY_HEIGHT {
+                for x in 0..DISPLAY_WIDTH {
+                    d.draw_rectangle(
+                        (x * DISPLAY_SCALE) as i32,
+                        (y * DISPLAY_SCALE) as i32,
+                        DISPLAY_SCALE as i32,
+                        DISPLAY_SCALE as i32,
+                        if self.display[y * DISPLAY_WIDTH + x] {
+                            Color::WHITE
+                        } else {
+                            Color::BLACK
+                        },
+                    );
                 }
-                0xE if nn == 0x9E && rl.is_key_down(code_to_key(v[x])) => self.pc += 2,
-                0xE if nn == 0x9E && !rl.is_key_down(code_to_key(v[x])) => {}
-                0xE if nn == 0xA1 && !rl.is_key_down(code_to_key(v[x])) => self.pc += 2,
-                0xE if nn == 0xA1 && rl.is_key_down(code_to_key(v[x])) => {}
-                0xF if nn == 0x0A => {
-                    if let Some(key) = rl.get_key_pressed() {
-                        v[x] = key_to_code(key);
-                        break;
-                    }
-                }
-                0xF if nn == 0x07 => v[x] = self.dt,
-                0xF if nn == 0x15 => self.dt = v[x],
-                0xF if nn == 0x18 => self.st = v[x],
-                0xF if nn == 0x1E => self.i += v[x] as usize,
-                0xF if nn == 0x33 => {
-                    let value = v[x];
-                    let hundreds = value / 100;
-                    let tens = (value / 10) % 10;
-                    let ones = value % 10;
-
-                    self.memory[self.i] = hundreds;
-                    self.memory[self.i + 1] = tens;
-                    self.memory[self.i + 2] = ones;
-                }
-                0xF if nn == 0x55 => self.memory[self.i..=self.i + x].copy_from_slice(&v[0..=x]),
-                0xF if nn == 0x65 => v[0..=x].copy_from_slice(&self.memory[self.i..=self.i + x]),
-                0xF if nn == 0x29 => self.i = v[x] as usize * 5,
-                _ => println!("Unknown instruction: {opcode:#X}"),
             }
 
-            self.pc += 2;
-
-            sleep(Duration::from_millis(1));
+            sleep(Duration::from_millis(16));
         }
     }
 
     fn should_run(&self) -> bool {
         self.pc < MEMORY_PROGRAM_START + self.program_len
+    }
+
+    fn execute_cycle(&mut self, rl: &mut RaylibHandle) {
+        let opcode = self.fetch_opcode();
+
+        let v = &mut self.registers;
+
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = ((opcode & 0x00F0) >> 4) as usize;
+        let n = opcode & 0x000F;
+        let nn = (opcode & 0x00FF) as u8;
+        let nnn = (opcode & 0x0FFF) as usize;
+
+        if self.dt > 0 {
+            self.dt -= 1;
+            return;
+        }
+
+        match (opcode & 0xF000) >> 12 {
+            0x0 if nn == 0x00 => {}
+            0x0 if nn == 0xE0 => self.display = [false; DISPLAY_WIDTH * DISPLAY_HEIGHT],
+            0x0 if nn == 0xEE => match self.stack.pop() {
+                Some(addr) => self.pc = addr,
+                None => println!("RET outside subroutine"),
+            },
+            0x1 => self.pc = nnn - 2,
+            0x2 => {
+                self.stack.push(self.pc);
+                self.pc = nnn - 2;
+            }
+            0x3 if v[x] == nn => self.pc += 2,
+            0x3 if v[x] != nn => {}
+            0x4 if v[x] != nn => self.pc += 2,
+            0x4 if v[x] == nn => {}
+            0x5 if n == 0 && v[x] == v[y] => self.pc += 2,
+            0x5 if n == 0 && v[x] != v[y] => {}
+            0x6 => v[x] = nn,
+            0x7 => v[x] = v[x].wrapping_add(nn),
+            0x8 if n == 0x0 => v[x] = v[y],
+            0x8 if n == 0x1 => v[x] |= v[y],
+            0x8 if n == 0x2 => v[x] &= v[y],
+            0x8 if n == 0x3 => v[x] ^= v[y],
+            0x8 if n == 0x4 => {
+                let (vx, carry) = v[x].overflowing_add(v[y]);
+                v[x] = vx;
+                v[0xF] = carry.into();
+            }
+            0x8 if n == 0x5 => {
+                let vf = (v[x] >= v[y]).into();
+                v[x] = v[x].wrapping_sub(v[y]);
+                v[0xF] = vf;
+            }
+            0x8 if n == 0x6 => {
+                let vf = v[y] & 1;
+                v[y] >>= 1;
+                v[0xF] = vf;
+            }
+            0x8 if n == 0x7 => {
+                let vf = (v[y] >= v[x]).into();
+                v[x] = v[y].wrapping_sub(v[x]);
+                v[0xF] = vf;
+            }
+            0x8 if n == 0xE => {
+                let vf = v[y] >> 7;
+                v[y] <<= 1;
+                v[0xF] = vf;
+            }
+            0x9 if n == 0x0 && v[x] != v[y] => self.pc += 2,
+            0x9 if n == 0x0 && v[x] == v[y] => {}
+            0xA => self.i = nnn,
+            0xB => self.pc = (nnn + v[0] as usize) - 2,
+            0xC => v[x] = fastrand::u8(0..u8::MAX) & nn,
+            0xD => {
+                v[0xF] = 0;
+                let x = v[x] as usize;
+                let y = v[y] as usize;
+
+                for row in 0..n {
+                    if (y + row as usize) >= DISPLAY_HEIGHT {
+                        break;
+                    }
+
+                    let sprite_byte = self.memory[self.i + row as usize];
+                    for bit in 0..8 {
+                        if (x + bit) >= DISPLAY_WIDTH {
+                            continue;
+                        }
+
+                        if (sprite_byte & (0x80 >> bit)) != 0 {
+                            let idx = (y + row as usize) * DISPLAY_WIDTH + (x + bit);
+                            if self.display[idx] {
+                                v[0xF] = 1;
+                            }
+                            self.display[idx] ^= true;
+                        }
+                    }
+                }
+            }
+            0xE if nn == 0x9E && rl.is_key_down(code_to_key(v[x])) => self.pc += 2,
+            0xE if nn == 0x9E && !rl.is_key_down(code_to_key(v[x])) => {}
+            0xE if nn == 0xA1 && !rl.is_key_down(code_to_key(v[x])) => self.pc += 2,
+            0xE if nn == 0xA1 && rl.is_key_down(code_to_key(v[x])) => {}
+            0xF if nn == 0x0A => {
+                if let Some(key) = rl.get_key_pressed() {
+                    v[x] = key_to_code(key);
+                    return;
+                }
+            }
+            0xF if nn == 0x07 => v[x] = self.dt,
+            0xF if nn == 0x15 => self.dt = v[x],
+            0xF if nn == 0x18 => self.st = v[x],
+            0xF if nn == 0x1E => self.i += v[x] as usize,
+            0xF if nn == 0x33 => {
+                let value = v[x];
+                let hundreds = value / 100;
+                let tens = (value / 10) % 10;
+                let ones = value % 10;
+
+                self.memory[self.i] = hundreds;
+                self.memory[self.i + 1] = tens;
+                self.memory[self.i + 2] = ones;
+            }
+            0xF if nn == 0x55 => self.memory[self.i..=self.i + x].copy_from_slice(&v[0..=x]),
+            0xF if nn == 0x65 => v[0..=x].copy_from_slice(&self.memory[self.i..=self.i + x]),
+            0xF if nn == 0x29 => self.i = v[x] as usize * 5,
+            _ => println!("Unknown instruction: {opcode:#X}"),
+        }
+
+        self.pc += 2;
     }
 
     fn fetch_opcode(&mut self) -> u16 {
